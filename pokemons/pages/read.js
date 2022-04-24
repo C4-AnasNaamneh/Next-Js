395d04ba4975050537dcbfaf6c899e049a84466b
AnasNaamneh <naamnih.anas@gmail.com>
2022-04-24 11:53:54 +0300

    Pulse Check Q1+Q2

---
+++ pokemons/pages/read.js
@@ -0,0 +1,24 @@
+//Pulse Check
+
+//1. As a website developer,Generally, when should I use CSR, and when to use SSR? (Interview question)
+
+//- we use CSR for interactive user interface (dynamic) , its faster than SSR after initial loading , because it doesn't update the whole html element tree ,it just update the state that has been changed 
+
+ //- we use SSR for static websites and when we want a better SEO results 
+
+
+ //2.What rendering method would work better for the these cases? and breifly explain why.
+
+//Complex games web application
+
+//CSR , because its interactive (dynamic) , and it require  fast rendering 
+
+//Newsletter website
+
+//SSR , because we are looking for better SEO results , and its a static website
+
+//Documentations website
+
+//SSR , because we are looking for better SEO results , and its a static website
+
+
