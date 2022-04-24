58448918ef383124c7f9ea911ce76cc535dfcd45
AnasNaamneh <naamnih.anas@gmail.com>
2022-04-24 17:16:45 +0300

    Practice Part I Q3

--- pokemons/pages/pokemons.js
+++ pokemons/pages/pokemons.js
@@ -1,9 +1,29 @@
 import styles from "../styles/Home.module.css";
 
-export default function pokemons() {
+export default function pokemons({ pokemonNames }) {
+  // console.log(pokemonNames);
+  //console.log(pokemonNames);
+
   return (
     <div className={styles.container}>
       <h1>Pokemons Page</h1>
+
+      {pokemonNames.game_indices.map((pokemonName) => (
+        <ul>
+          <li> {pokemonName.version.name}</li>
+        </ul>
+      ))}
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
+  const pokemonNames = await res.json();
+
+  return {
+    props: {
+      pokemonNames,
+    },
+  };
 }
